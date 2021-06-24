88f981851fd18f0369fff2bd0a6572d04ffb4769
Valentine <valentinezavizion@gmail.com>
2021-06-24 13:20:19 +0300

    Something new

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-const TelegramBot = require('node-telegram-bot-api');
-const token = '1879933872:AAFq_UDOoFlQo7JwfwLyFPpPRoUMhsFc7J4';
-const bot = new TelegramBot(token, { polling: true });
+const Telegraf = require('telegraf').Telegraf;
+const bot = new Telegraf('1879933872:AAFq_UDOoFlQo7JwfwLyFPpPRoUMhsFc7J4');
+const request = require('request');
 
-bot.on('message', msg => {
-    let Hi = 'hi';
-    let Create = '/create';
-    let Start = '/start';
-
-    let userData = { login: '', pass: '' };
-
-    if(msg.text.toString().toLowerCase().indexOf(Hi) === 0) {
-        bot.sendMessage(msg.chat.id, 'Hello!');
-    }
-
-    if(msg.text.toString().toLowerCase().indexOf(Create) === 0) {
-        let req = (HttpWebRequest)(HttpWebRequest.Create('https://expensesapp2-dev-ed.lightning.force.com/lightning/o/Expense_Card__c'))
-    }
-
-    if(msg.text.toString().toLowerCase().indexOf(Start) === 0) {
-        bot.sendMessage(msg.chat.id, 'Login:');
-    }
+bot.command('start', msg => {
+    console.log(msg.from);
+    request('https://expensesapp2-dev-ed.lightning.force.com', (err, res, body) => {
+        if(err) {
+            console.log(err)
+        } else {
+            console.log(body);
+        }
+    })
+    bot.telegram.sendMessage(msg.chat.id, 'hello there! Welcome to my new telegram bot.' + msg.from.first_name, {
+    })
 })
+
+
+
+
+
+
+bot.launch();
+//  1879933872:AAFq_UDOoFlQo7JwfwLyFPpPRoUMhsFc7J4
