4df378c2ef2e0171959c2d14aee3d4c9b61af9c3
Valentine <valentinezavizion@gmail.com>
2021-06-24 13:48:02 +0300

    Do something with endpoint and auth

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 const Telegraf = require('telegraf').Telegraf;
 const bot = new Telegraf('1879933872:AAFq_UDOoFlQo7JwfwLyFPpPRoUMhsFc7J4');
-const request = require('request');
+const https = require('https');
+
+const data = JSON.stringify({
+    Amount__c: 100
+  })
+
+const options = {
+    hostname: 'https://expensesapp2-dev-ed.lightning.force.com/services/data/v52.0/sobjects/Expense_Card__c',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': data.length
+    }
+  }
 
 bot.command('start', msg => {
     console.log(msg.from);
-    request('https://expensesapp2-dev-ed.lightning.force.com', (err, res, body) => {
-        if(err) {
-            console.log(err)
-        } else {
-            console.log(body);
-        }
+    let url = 'https://expensesapp2-dev-ed.lightning.force.com/services/data/v52.0/sobjects/Expense_Card__c/'
+    const req = https.request(options, res => {
+        console.log(`statusCode: ${res.statusCode}`)
+        
+        res.on('data', d => {
+            process.stdout.write(d)
+        })
     })
+
+    req.on('error', error => {
+    console.error(error)
+    })
+
+    req.write(data)
+    req.end()
+
     bot.telegram.sendMessage(msg.chat.id, 'hello there! Welcome to my new telegram bot.' + msg.from.first_name, {
     })
 })
